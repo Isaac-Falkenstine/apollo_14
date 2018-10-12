b0c2c13b44ae6eadb84b6a23823fefbf68d33fb1
Isaac Falkenstine <isaac@falkenstine.com>
2018-10-12 10:32:40 -0600

    Finished first 2 user stories (Forgot to commit before)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Astronaut.create(name: 'Buzz Aldrin', age: '88', job: "Command Pilot")
+Astronaut.create(name: 'John Glenn', age: '95 (At Death)', job: "Astronaut")
+Astronaut.create(name: 'Neil Armstrong', age: '82 (At Death)', job: "Test Pilot (First man on moon)")
+Astronaut.create(name: 'James Lovell Jr.', age: '90', job: "Astronaut")
+Astronaut.create(name: 'Chris Hadfield', age: '59', job: "Canadian Astronaut")
